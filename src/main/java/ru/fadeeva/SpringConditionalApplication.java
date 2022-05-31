bc8646965fb0b204edca69befed480ca099b017a
Margarita Fadeeva <morganGTO@yandex.ru>
2022-05-31 21:18:14 +0300

    Init

---
+++ src/main/java/ru/fadeeva/SpringConditionalApplication.java
@@ -0,0 +1,13 @@
+package ru.fadeeva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConditionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConditionalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/fadeeva/profile/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.fadeeva.profile;
+
+public interface SystemProfile {
+    String getProfile();
+}
